28e96a5dfcbf68116b7d10caf481006db67f31ab
Lucas Magno <lucasmagno695@gmail.com>
2021-11-20 13:19:37 -0300

    primeiro commit

---
+++ src/components/subscribeButton/index.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss';
+
+interface subscribeButtonProps{
+ priceId: string;
+
+}
+
+export function SubscribeButton({priceId}:subscribeButtonProps ) {
+    return (
+        <button
+            type='button'
+            className={styles.subscribeButton}
+
+        >
+            Subscribe Now
+        </button>
+    );
+}
