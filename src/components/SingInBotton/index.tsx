9728d9cef0d84432853da8a1a4eb3f90fe260a6d
Lucas Magno <lucasmagno695@gmail.com>
2021-11-24 13:07:54 -0300

    atualizações

--- src/components/SingInBotton/index.tsx
+++ src/components/SingInBotton/index.tsx
@@ -1,24 +1,29 @@
 import styles from './styles.module.scss';
 
 import { FaGithub } from 'react-icons/fa';
-import {FiX} from 'react-icons/fi';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button
             type="button"
-            className={styles.signInButton}>
+            className={styles.signInButton}
+            onClick={() => signOut()}
+        >
 
             <FaGithub color="#04d361" />
-            Lucas Magno
-            <FiX color="#737380" className={styles.closeIcon}/>
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button
             type="button"
-            className={styles.signInButton}>
+            className={styles.signInButton}
+            onClick={() => signIn('github')}
+        >
 
             <FaGithub color="#eba417" />
             Sign in with Github
